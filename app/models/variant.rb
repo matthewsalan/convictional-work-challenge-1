64f0ea6f54e402a44143fde6621a33e7be2c4cb7
Alan Matthews <matthewsa1320@gmail.com>
2024-01-07 18:14:06 -0500

    set up docker

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -2,4 +2,6 @@ class Variant < ApplicationRecord
   belongs_to :product
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images
+  validates :title, presence: true, uniqueness: true
+  validates :sku, presence: true, uniqueness: true
 end
